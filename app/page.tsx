09edaae7de5a7f405da1190fa5fa54f4dc89ca79
@Rein-1 <markreniermercado123@gmail.com>
2024-11-05 23:39:24 +0800

    added background Image

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import TimeDate from "./components/TimeDate";
 import Notif from "./components/Notif";
 import News from "./components/News";
 
-
 export default function Home() {
   return (
     <main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        lockscreen: "url('/assets/bgImage.jpg')"
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
