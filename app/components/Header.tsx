a8282d4a29d24b45e11e666462f6f3222d16a2b4
@Rein-1 <markreniermercado123@gmail.com>
2024-11-05 23:14:17 +0800

    remodify

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,19 +1,23 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react'
+import React from "react";
+import { BsGlobe } from "react-icons/bs";
 
 const Nav = () => {
   return (
-    <div className='md:flex flex-wrap justify-center md:justify-between space-y-4 md:space-y-0 mx-20 my-10 min-w-max border'>
-      <div className='grid justify-items-center w-[270px] gap-4  border'>
-        <img src="" alt="Browser" className='border' />
-        <p className=' border'>The Fastest most intelligent Windows PC&lsquo;s ever. A new AI era begins.</p>
-      </div>  
-      <div className='grid justify-items-center w-[270px] gap-4 border'>
-        <img src="" alt="Camera" className='border' />
-        <p className='border'>Like the image that you see?</p>
+    <div className="md:flex flex-wrap justify-center md:justify-between space-y-4 md:space-y-0 mx-20 my-10 min-w-max border">
+      <div className="grid justify-items-center w-[270px] gap-4  border">
+        <BsGlobe className="w-6 h-6" />
+        <p className=" border">
+          The Fastest most intelligent Windows PC&lsquo;s ever. A new AI era
+          begins.
+        </p>
+      </div>
+      <div className="grid justify-items-center w-[270px] gap-4 border">
+        <img src="" alt="Camera" className="border" />
+        <p className="border">Like the image that you see?</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
--- app/components/Notif.tsx
+++ app/components/Notif.tsx
@@ -1,11 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
-import { BsGlobe } from "react-icons/bs";
-
 
 const Notif = () => {
   return (
     <div className='grid flex-col justify-center gap-2 my-52 border'>
-        <BsGlobe />
+        <img src="" alt="Browser" className='border'/>
         <span className='border'>Notification</span>
     </div>
   )
