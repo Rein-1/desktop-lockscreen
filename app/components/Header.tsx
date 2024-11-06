721c3fd312fcf2b725d1608d450f467063364ec4
@Rein-1 <markreniermercado123@gmail.com>
2024-11-06 22:29:05 +0800

    update header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -7,14 +7,14 @@ const Nav = () => {
   return (
     <div className="md:flex flex-wrap justify-center md:justify-between space-y-4 md:space-y-0 mx-20 my-10 min-w-max border">
       <div className="grid justify-items-center w-[270px] gap-4  border">
-        <BsGlobe className="w-6 h-6" />
+        <BsGlobe className="w-10 h-10 rounded-xl bg-opacity-50 bg-black p-2" />
         <p className="font-medium border">
           The Fastest most intelligent Windows PC&lsquo;s ever. A new AI era
           begins.
         </p>
       </div>
       <div className="grid justify-items-center w-[270px] gap-4 border">
-      <MdOutlineCamera className="w-7 h-7" />
+      <MdOutlineCamera className="w-11 h-11 rounded-xl bg-opacity-50 bg-black p-2" />
         <p className="font-medium border">Like the image that you see?</p>
       </div>
     </div>
