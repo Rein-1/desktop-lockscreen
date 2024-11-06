4867943797de9658e3728f03fee58dfb821777cd
@Rein-1 <markreniermercado123@gmail.com>
2024-11-06 21:35:06 +0800

    add time

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,14 +8,14 @@ const Nav = () => {
     <div className="md:flex flex-wrap justify-center md:justify-between space-y-4 md:space-y-0 mx-20 my-10 min-w-max border">
       <div className="grid justify-items-center w-[270px] gap-4  border">
         <BsGlobe className="w-6 h-6" />
-        <p className=" border">
+        <p className="font-medium border">
           The Fastest most intelligent Windows PC&lsquo;s ever. A new AI era
           begins.
         </p>
       </div>
       <div className="grid justify-items-center w-[270px] gap-4 border">
       <MdOutlineCamera className="w-7 h-7" />
-        <p className="border">Like the image that you see?</p>
+        <p className="font-medium border">Like the image that you see?</p>
       </div>
     </div>
   );
--- app/components/TimeDate.tsx
+++ app/components/TimeDate.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const TimeDate = () => {
+  const time = new Date().toLocaleTimeString()
+  console.log(time)
+
   return (
     <div className='grid justify-center flex-col border gap-4'>
-      <div className='border'>Time</div>
+      <div className='border'>{time}</div>
       <div className='border'>Date</div>
     </div>
   )
