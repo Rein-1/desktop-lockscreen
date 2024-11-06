ad5b37a4ed939af11a595349d01a3fe781a7f04b
@Rein-1 <markreniermercado123@gmail.com>
2024-11-06 22:12:58 +0800

    update date and time

--- app/components/TimeDate.tsx
+++ app/components/TimeDate.tsx
@@ -1,13 +1,36 @@
-import React from 'react'
+'use client'
+import { useEffect, useState } from "react"
 
 const TimeDate = () => {
-  const time = new Date().toLocaleTimeString()
-  console.log(time)
+  const [time, setTime] = useState(new Date().toLocaleTimeString()) 
+  const [date, setDate] = useState(new Date().toLocaleDateString( 'en-US', {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric'
+  }))
+  useEffect(() => {
+    const IntervalId = setInterval(() => {
+      setTime(new Date().toLocaleTimeString())
+      setDate(new Date().toLocaleDateString( 'en-US', {
+        weekday: 'long',
+        month: 'long',
+        day: 'numeric'
+      }))
+    }, 1000)
+
+    return () => clearInterval(IntervalId)
+  }, [])
+  // console.log(time)
+
+ 
+
+
+  console.log(date)
 
   return (
     <div className='grid justify-center flex-col border gap-4'>
-      <div className='border'>{time}</div>
-      <div className='border'>Date</div>
+      <div className='flex justify-center font-bold text-4xl text-black border'>{time}</div>
+      <div className='font-bold text-3xl text-black border'>{date}</div>
     </div>
   )
 }
