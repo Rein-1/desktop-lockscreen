67f81b0bbd96133d6f66f67d8abaa46b4f21681f
@Rein-1 <markreniermercado123@gmail.com>
2024-11-08 21:26:32 +0800

    some change

--- app/components/Notif.tsx
+++ app/components/Notif.tsx
@@ -1,14 +1,18 @@
 /* eslint-disable @next/next/no-img-element */
 
-import { IoSearch } from "react-icons/io5"
+import { IoSearch } from "react-icons/io5";
 
 const Notif = () => {
   return (
-    <div className='grid flex-col justify-center gap-2 my-52 border'>
-        <IoSearch className="w-7 h-7 rounded-lg bg-black bg-opacity-80"/>
-        <span className='border'>Notification</span>
+    <div className="flex flex-col items-center justify-center gap-2 my-20 border">
+      <IoSearch className="w-7 h-7 rounded-lg bg-black bg-opacity-80" />
+      <span className="flex-wrap w-80 border">
+        The AI era is a time when artificial intelligence (AI) is rapidly
+        advancing and transforming how humans interact with machines and live
+        their lives.
+      </span>
     </div>
-  )
-}
+  );
+};
 
-export default Notif
+export default Notif;
